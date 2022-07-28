4ce3e698c5c98854da92a682f5ef9c6ddf959da2
sandaru-luckjaya99 <96827@fhss.sjp.ac.lk>
2022-07-28 16:03:02 +0530

    navigation bar hamburgar menu icon

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -9,7 +9,10 @@ export default function Navbar() {
                 <a href="#!">Reciepies</a>
                 <a href="#!">Settings</a>
             </div>
-            <div className="side">
+            <div className="sidebar-btn">
+                <div className="bar"></div>
+                <div className="bar"></div>
+                <div className="bar"></div>
 
             </div>
         </div>
