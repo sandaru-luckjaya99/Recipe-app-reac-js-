1dd60c4953a67a886be3febbdb060cc247a2848d
sandaru-luckjaya99 <96827@fhss.sjp.ac.lk>
2022-07-28 12:30:19 +0530

    initial commit

---
+++ src/component/Navbar.js
@@ -0,0 +1,17 @@
+export default function Navbar() {
+    return (
+        <div className="navbar container">
+            <a href="#!" className="logo">
+                F<span>oo</span>dshub
+            </a>
+            <div className="nav-links">
+                <a href="#!">Home</a>
+                <a href="#!">Reciepies</a>
+                <a href="#!">Settings</a>
+            </div>
+            <div className="side">
+
+            </div>
+        </div>
+    );
+}
