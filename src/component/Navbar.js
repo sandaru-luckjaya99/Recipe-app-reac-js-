92edddf1b355f5a87e71ecc302878e6e2c450e17
sandaru-luckjaya99 <96827@fhss.sjp.ac.lk>
2022-07-29 21:06:11 +0530

    side bar initialize

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,38 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
+import Sidebar from "./Sidebar";
 
 export default function Navbar() {
+  const [showSidebar, setshowSidebar] = useState(false);
 
-    const [showSidebar,setshowSidebar]=useState(false)
+  // function addCount(){
+  //     setcount(Count=>Count + 1)
+  // }
+  // function deCount(){
+  //     setcount(Count=>Count - 1)
+  // }
 
-    // function addCount(){
-    //     setcount(Count=>Count + 1)
-    // }
-    // function deCount(){
-    //     setcount(Count=>Count - 1)
-    // }
-
-    return (
-        
-        <div className="navbar container">
-            <a href="#!" className="logo">
-                F<span>oo</span>dshub
-            </a>
-            <div className="nav-links">
-                <a href="#!">Home</a>
-                <a href="#!">Reciepies</a>
-                <a href="#!">Settings</a>
-            </div>
-
-            {/* function to show or not side bar */}
-            <div onClick={()=>setshowSidebar(!showSidebar)} className= {showSidebar ? "sidebar-btn active" :"sidebar-btn"}>
-                <div className="bar"></div>
-                <div className="bar"></div>
-                <div className="bar"></div>
-
-            </div>
+  return (
+    <>
+      <div className="navbar container">
+        <a href="#!" className="logo">
+          F<span>oo</span>dshub
+        </a>
+        <div className="nav-links">
+          <a href="#!">Home</a>
+          <a href="#!">Reciepies</a>
+          <a href="#!">Settings</a>
         </div>
 
-        
-    );
+        {/* function to show or not side bar */}
+        <div
+          onClick={() => setshowSidebar(!showSidebar)}
+          className={showSidebar ? "sidebar-btn active" : "sidebar-btn"}
+        >
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+      </div>
+      <Sidebar></Sidebar>
+    </>
+  );
 }
