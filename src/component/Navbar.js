a114ec27c29207cd4564f79a9c82e6ab957bb4e1
sandaru-luckjaya99 <96827@fhss.sjp.ac.lk>
2022-07-29 21:39:13 +0530

    side bar  icons

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,62 +1,65 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-
 import Sidebar from "./Sidebar";
 
+import { faHome, faList, faCog } from "@fortawesome/free-solid-svg-icons";
+
 export default function Navbar() {
-  const [showSidebar, setshowSidebar] = useState(false);
+    const [showSidebar, setshowSidebar] = useState(false);
 
-  const links = [
-    {
-      name: "home",
-      path: "/",
-    },
-    {
-      name: "Recipes",
-      path: "/recipes",
-    },
-    {
-      name: "Settings",
-      path: "/Settings",
-    },
-  ];
+    const links = [
+        {
+            name: "home",
+            path: "/",
+            icon: faHome
+        },
+        {
+            name: "Recipes",
+            path: "/recipes",
+            icon: faList
+        },
+        {
+            name: "Settings",
+            path: "/Settings",
+            icon: faCog
+        },
+    ];
 
-  // function addCount(){
-  //     setcount(Count=>Count + 1)
-  // }
-  // function deCount(){
-  //     setcount(Count=>Count - 1)
-  // }
+    // function addCount(){
+    //     setcount(Count=>Count + 1)
+    // }
+    // function deCount(){
+    //     setcount(Count=>Count - 1)
+    // }
 
-  return (
-    <>
-      <div className="navbar container">
-        <a href="#!" className="logo">
-          F<span>oo</span>dshub
-        </a>
-        <div className="nav-links">
-          {links.map((link) => (
-            <a href="#!" key={link.name}>
-              {link.key}
-            </a>
-          ))}
+    return (
+        <>
+            <div className="navbar container">
+                <a href="#!" className="logo">
+                    F<span>oo</span>dshub
+                </a>
+                <div className="nav-links">
+                    {links.map((link) => (
+                        <a href="#!" key={link.name}>
+                            {link.key}
+                        </a>
+                    ))}
 
-          {/* <a href="#!">Home</a>
+                    {/* <a href="#!">Home</a>
           <a href="#!">Reciepies</a>
           <a href="#!">Settings</a> */}
-        </div>
+                </div>
 
-        {/* function to show or not side bar */}
-        <div
-          onClick={() => setshowSidebar(!showSidebar)}
-          className={showSidebar ? "sidebar-btn active" : "sidebar-btn"}
-        >
-          <div className="bar"></div>
-          <div className="bar"></div>
-          <div className="bar"></div>
-        </div>
-      </div>
-      <Sidebar links={links} />
-    </>
-  );
+                {/* function to show or not side bar */}
+                <div
+                    onClick={() => setshowSidebar(!showSidebar)}
+                    className={showSidebar ? "sidebar-btn active" : "sidebar-btn"}
+                >
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                </div>
+            </div>
+            <Sidebar links={links} />
+        </>
+    );
 }
--- src/component/Sidebar.js
+++ src/component/Sidebar.js
@@ -1,9 +1,12 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export default function Sidebar({ links }) {
   return (
     <div className="sidebar">
 
       {links.map((link) => (
-        <a href="#!" key={link.name}>
+        <a className="sidebar-links" href="#!" key={link.name}>
+          <FontAwesomeIcon icon={link.icon}></FontAwesomeIcon>
           {link.name}
         </a>
       ))}
