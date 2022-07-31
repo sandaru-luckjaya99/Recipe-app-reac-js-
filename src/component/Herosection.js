22c694a57df234f6c9f2d047c2e825c4e78f3f91
sandaru-luckjaya99 <96827@fhss.sjp.ac.lk>
2022-07-31 20:43:38 +0530

    about section with scss

--- src/component/Herosection.js
+++ src/component/Herosection.js
@@ -1,16 +1,24 @@
-export default function Herosection(){
-
-  return(
+export default function Herosection() {
+  return (
     <div className="section">
+      <div className="col">
+        <h1 className="title"> What are we about</h1>
+        <p className="info">
+          FoodieHub offers very cost-effective solutions to restaurants by
+          charging 0% commission on each food order. FoodieHub is a
+          subscription-based modal in that restaurant only pays a very minimum
+          {/* amount per month or quarter or half-year, or year. Customers also get
+          their delicious food delivered or picked up at a very reasonable cost
+          as restaurants do not need to charge an extra amount on food due to a
+          0% commission on the order. FoodieHub offers 30 days free trial to
+          restaurants. After the expiry of the free trial, the restaurant has to
+          buy any of subscriptions monthly, quarterly, half-yearly, or yearly. */}
+        </p>
+        <button className="btn"> Exploe Now </button>
 
-        <div className="col"> 
-          col1
-        </div>
+      </div>
 
-        <div className="col">
-          col2
-        </div>
-
+      <div className="col">col2</div>
     </div>
-  )
+  );
 }
