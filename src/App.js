80ea1e0f3c0da63262ddf722dc8a4e7fd905d0e6
sandaru-luckjaya99 <96827@fhss.sjp.ac.lk>
2022-07-31 20:48:56 +0530

    default section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Navbar from "./component/Navbar";
+import Herosection from "./component/Herosection";
 
 
 function App() {
   return (
     <div className="">
       <Navbar />
+      <div className="container main">
+        <Herosection/>
+      </div>
     </div>
   );
 }
