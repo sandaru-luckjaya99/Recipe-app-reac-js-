8c131fa10c7c3fda8e4fd486f0c6edf289f3006b
sandaru-luckjaya99 <96827@fhss.sjp.ac.lk>
2022-07-31 14:43:34 +0530

    default section created

---
+++ src/component/Herosection.js
@@ -0,0 +1,16 @@
+export default function Herosection(){
+
+  return(
+    <div className="section">
+
+        <div className="col"> 
+          col1
+        </div>
+
+        <div className="col">
+          col2
+        </div>
+
+    </div>
+  )
+}
